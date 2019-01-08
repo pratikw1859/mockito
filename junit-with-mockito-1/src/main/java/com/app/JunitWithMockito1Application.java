fc91d39f8998236f39c832ad9e0ec26b2d390a24
pratikw1859 <pratikwankhede814@gmail.com>
2019-01-08 18:32:09 +0530

    first commit

---
+++ junit-with-mockito-1/src/main/java/com/app/JunitWithMockito1Application.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitWithMockito1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitWithMockito1Application.class, args);
+	}
+
+}
+
